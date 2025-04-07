7036d6649c4b84b18475da1f21ae4e3f14dedba8
jasur@gmail.com <jasur@gmail.com>
2025-04-07 19:06:56 +0500

    vazifa2

--- js/data.js
+++ js/data.js
@@ -1,5 +1,10 @@
 export default [
   {
+    title: "example 2",
+    github: "https://github.com/Feaz13000",
+    vercel: "https://vercel.com/feaz13000s-projects",
+  },
+  {
     title: "Example",
     github: "example.uz",
     vercel: "example.uz",
