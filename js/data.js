ab10a51067722fd6d99dba28aeca57dea9f6f289
jasur@gmail.com <jasur@gmail.com>
2025-04-10 23:16:56 +0500

    davomi

--- js/data.js
+++ js/data.js
@@ -1,11 +1,16 @@
 export default [
   {
+    title: "example 3",
+    github: "https://github.com/Feaz13000/9dars",
+    vercel: "https://vercel.com/feaz13000s-projects/9dars",
+  },
+  {
     title: "example 2",
     github: "https://github.com/Feaz13000/8dars",
     vercel: "https://vercel.com/feaz13000s-projects/8dars",
   },
   {
-    title: "example 2",
+    title: "example 1",
     github: "https://github.com/Feaz13000/portfolio",
     vercel: "https://portfolio-rouge-two-79.vercel.app/",
   },
