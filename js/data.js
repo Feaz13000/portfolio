78388b9cb335be9a4b4f5fbed74e63af7c7658dd
jasur@gmail.com <jasur@gmail.com>
2025-04-17 22:45:01 +0500

    davomi2

--- js/data.js
+++ js/data.js
@@ -1,5 +1,20 @@
 export default [
   {
+    title: "example 6",
+    github: "https://github.com/Feaz13000/homevork2",
+    vercel: "https://homevork2-opal.vercel.app/",
+  },
+  {
+    title: "example 5",
+    github: "https://github.com/Feaz13000/homewor",
+    vercel: "https://homewor-five.vercel.app/",
+  },
+  {
+    title: "example 4",
+    github: "https://github.com/Feaz13000/11dars",
+    vercel: "https://vercel.com/feaz13000s-projects/11dars-fdfb",
+  },
+  {
     title: "example 3",
     github: "https://github.com/Feaz13000/9dars",
     vercel: "https://vercel.com/feaz13000s-projects/9dars",
@@ -8,11 +23,6 @@ export default [
     title: "example 2",
     github: "https://github.com/Feaz13000/8dars",
     vercel: "https://vercel.com/feaz13000s-projects/8dars",
-  },
-  {
-    title: "example 1",
-    github: "https://github.com/Feaz13000/portfolio",
-    vercel: "https://portfolio-rouge-two-79.vercel.app/",
   },
   {
     title: "Example",
