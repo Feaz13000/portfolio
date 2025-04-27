f485925c63e28dc0083e039db3178708011ec51f
jasur@gmail.com <jasur@gmail.com>
2025-04-27 12:11:47 +0500

    davomi

--- js/data.js
+++ js/data.js
@@ -1,5 +1,20 @@
 export default [
   {
+    title: "example 8 csshomweork3",
+    github: "https://github.com/Feaz13000/homework3",
+    vercel: "https://vercel.com/new/feaz13000s-projects/success?developer-id=&external-id=&redirect-url=&branch=main&deploymentUrl=homework3-rnhp2yde1-feaz13000s-projects.vercel.app&projectName=homework3&s=https%3A%2F%2Fgithub.com%2FFeaz13000%2Fhomework3&gitOrgLimit=&hasTrialAvailable=1&totalProjects=1&flow-id=LYU4pESpQebiVglrZ4Iwq",
+    },
+    {
+    title: "example 8 csshomweork2",
+    github: "https://github.com/Feaz13000/homework2",
+    vercel: "https://vercel.com/new/feaz13000s-projects/success?developer-id=&external-id=&redirect-url=&branch=main&deploymentUrl=homework2-bav3hwuu0-feaz13000s-projects.vercel.app&projectName=homework2&s=https%3A%2F%2Fgithub.com%2FFeaz13000%2Fhomework2&gitOrgLimit=&hasTrialAvailable=1&totalProjects=1&flow-id=xYeDiCA3fvI4F-86gZR8t",
+    },
+  {
+    title: "example 7 csshomeworkanimation1",
+    github: "https://github.com/Feaz13000/homework-3dars",
+    vercel: "https://vercel.com/new/feaz13000s-projects/success?developer-id=&external-id=&redirect-url=&branch=main&deploymentUrl=homework-3dars-5x8bzda6m-feaz13000s-projects.vercel.app&projectName=homework-3dars&s=https%3A%2F%2Fgithub.com%2FFeaz13000%2Fhomework-3dars&gitOrgLimit=&hasTrialAvailable=1&totalProjects=1&flow-id=jysxWRSL0yvm69AE4Moqj",
+  },
+  {
     title: "example 6",
     github: "https://github.com/Feaz13000/homevork2",
     vercel: "https://homevork2-opal.vercel.app/",
